49596e8b39f7abeb96d0f670e39093432b4f58ff
Sankalp125 <sankalp.tiwari125@gmail.com>
2026-03-12 15:51:21 +0530

    update profile picture reponse updated

--- src/main/kotlin/routing/ProtectedRoutes.kt
+++ src/main/kotlin/routing/ProtectedRoutes.kt
@@ -162,8 +162,8 @@ fun Routing.protectedRoutes() {
                             }
                         } else {
                             try {
-                                UserRepository.updateProfilePicture(UUID.fromString(userId), profilePictureUrl)
-                                call.respond(HttpStatusCode.OK, mapOf("message" to "Profile picture updated successfully"))
+                                val url = UserRepository.updateProfilePicture(UUID.fromString(userId), profilePictureUrl)
+                                call.respond(HttpStatusCode.OK, mapOf("url" to url))
                             } catch (e: Exception) {
                                 return@put call.respond(
                                     HttpStatusCode.InternalServerError,
